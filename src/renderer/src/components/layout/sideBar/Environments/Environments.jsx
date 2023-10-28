40470b9af248a8778b4ebe83f678ef769ba6c396
Francesc Requesens <francesc.requesens@gmail.com>
2023-10-28 16:25:15 +0200

    environment styles

--- src/renderer/src/components/layout/sideBar/Environments/Environments.jsx
+++ src/renderer/src/components/layout/sideBar/Environments/Environments.jsx
@@ -12,23 +12,26 @@ export default function Environments() {
   return (
     <>
       <div className={styles.header}>
-        { selectedEnvironment && (
-          <div className={styles.back}>
-            <ButtonIcon icon='back' onClick={() => setSelectedEnvironment(null)} />
-          </div>
-        )}
         <div className={styles.title}>
           Environments
         </div>
       </div>
       { selectedEnvironment && (
         <div className={styles.selectedEnvironment}>
-          <div className={styles.title}>{selectedEnvironment.name}</div>
+          <div className={styles.selectedEnvironmentHeader}>
+            <div className={styles.back}>
+              <ButtonIcon icon='back' onClick={() => setSelectedEnvironment(null)} />
+            </div>
+            <div className={styles.title}>{selectedEnvironment.name}</div>
+          </div>
           <div className={styles.content}>
             {selectedEnvironment.variables.map((variable, index) => (
               <div className={styles.variable} key={index}>
-                <div className={styles.name}>{variable.name}</div>
-                <div className={styles.value}>{variable.value}</div>
+                <input className={styles.name} value={variable.name} />
+                <input className={styles.value} value={variable.value} />
+                <div>
+                  <ButtonIcon icon='delete' />
+                </div>
               </div>
             ))}
           </div>
