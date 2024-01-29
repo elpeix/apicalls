6ac1ac9cb09f34bb624d6beb7312764d11ece9be
Francesc d'Assís Requesens i Roca <francesc.requesens@gmail.com>
2024-01-29 17:03:10 +0100

    Add EditableName component

---
+++ src/renderer/src/components/base/EditableName/EditableName.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useRef, useState } from 'react'
+import styles from './EditableName.module.css'
+
+export default function EditableName({
+  name,
+  editMode = false,
+  update,
+  onBlur
+}: {
+  name: string
+  editMode: boolean
+  update: (name: string) => void
+  onBlur?: () => void
+}) {
+  const [editingName, setEditingName] = useState(false)
+  const [nameValue, setNameValue] = useState(name)
+  const nameRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (editMode) {
+      setEditingName(true)
+      setTimeout(() => {
+        if (!nameRef.current) return
+        nameRef.current.setSelectionRange(0, nameValue.length)
+        nameRef.current.focus()
+      }, 0)
+    }
+  }, [editMode])
+
+  const editName = () => {
+    setEditingName(true)
+    setNameValue(name)
+    setTimeout(() => {
+      if (!nameRef.current) return
+      nameRef.current.setSelectionRange(0, nameValue.length)
+      nameRef.current.focus()
+    }, 0)
+  }
+  const changeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNameValue(e.target.value)
+  }
+  const onKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === 'Escape') {
+      setEditingName(false)
+      setNameValue(name)
+      if (onBlur) onBlur()
+    }
+    if (e.key === 'Enter') {
+      setEditingName(false)
+      update(nameValue)
+    }
+  }
+
+  const handleOnBlur = () => {
+    setEditingName(false)
+    update(nameValue)
+    if (onBlur) onBlur()
+  }
+
+  return (
+    <div className={`${styles.name} ${editingName && styles.editable}`} onDoubleClick={editName}>
+      {editingName && (
+        <input
+          ref={nameRef}
+          className={styles.nameInput}
+          placeholder="Collection name"
+          value={nameValue}
+          onChange={changeName}
+          onBlur={handleOnBlur}
+          onKeyDown={onKeyDown}
+          autoFocus
+        />
+      )}
+      {!editingName && nameValue}
+    </div>
+  )
+}
