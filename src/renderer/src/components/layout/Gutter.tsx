92c5186c9093b0ee1d4bbd8894b74e6078962e5e
Francesc d'Assís Requesens i Roca <francesc.requesens@gmail.com>
2024-02-13 19:00:35 +0100

    Refactor Gutter component styles

--- src/renderer/src/components/layout/Gutter.tsx
+++ src/renderer/src/components/layout/Gutter.tsx
@@ -2,22 +2,21 @@ import React, { useState } from 'react'
 import { PanelResizeHandle } from 'react-resizable-panels'
 import styles from './Gutter.module.css'
 
-export default function Gutter({ mode, onDoubleClick }: {
-  mode: 'horizontal' | 'vertical',
+export default function Gutter({
+  mode,
+  onDoubleClick
+}: {
+  mode: 'horizontal' | 'vertical'
   onDoubleClick: () => void
 }) {
-
-  if ([ 'horizontal', 'vertical' ].indexOf(mode) === -1) {
+  if (['horizontal', 'vertical'].indexOf(mode) === -1) {
     throw new Error('Invalid mode')
   }
-
   const [isDragging, setIsDragging] = useState(false)
-  
+  const className = `${styles.gutter} ${styles[mode]} ${isDragging ? styles.drag : ''}`
+
   return (
-    <PanelResizeHandle 
-      className={`${styles.gutter} ${styles[mode]} ${isDragging ? styles.drag : ''}`}
-      onDragging={setIsDragging} 
-    >
+    <PanelResizeHandle className={className} onDragging={setIsDragging}>
       <div className={styles.inset} onDoubleClick={onDoubleClick} />
     </PanelResizeHandle>
   )
