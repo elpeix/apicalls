53be2d2f21815e017504a8383cece4075a3fcb02
Francesc Requesens <francesc.requesens@gmail.com>
2024-04-25 19:39:55 +0200

    Persist sidebar tabs

--- src/renderer/src/components/layout/sideBar/SidePanel/SidePanel.tsx
+++ src/renderer/src/components/layout/sideBar/SidePanel/SidePanel.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import styles from './SidePanel.module.css'
 import { AppContext } from '../../../../context/AppContext'
 import History from '../history/History'
@@ -8,16 +8,27 @@ import Collections from '../Collections/Collections'
 
 export default function SidePanel() {
   const { menu } = useContext(AppContext)
+  let elements = {}
+
+  useEffect(() => {
+    const collections = <Collections />
+    const environments = <Environments />
+    const history = <History />
+    const settings = <Settings />
+    elements = { collections, environments, history, settings }
+  }, [])
+
+  const classNameSelected = menu?.selected?.id || ''
 
   return (
     <aside className={styles.sidePanel}>
       <div className={styles.content}>
-        {menu && menu.selected && (
-          <div className={styles.sidePanel}>
-            {menu.selected.id === 'collection' && <Collections />}
-            {menu.selected.id === 'environment' && <Environments />}
-            {menu.selected.id === 'history' && <History />}
-            {menu.selected.id === 'settings' && <Settings />}
+        {menu && (
+          <div className={`${styles.sidePanel} ${styles[classNameSelected]}`}>
+            <div className={styles.collections}><Collections /></div>
+            <div className={styles.environment}><Environments /></div>
+            <div className={styles.history}><History /></div>
+            <div className={styles.settings}><Settings /></div>
           </div>
         )}
       </div>
