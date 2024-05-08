3a65de90cf39d4f1c53e9ea06546bc809fccd3c1
Francesc d'Assís Requesens i Roca <francesc.requesens@gmail.com>
2024-05-08 19:39:07 +0200

    Add autoFocus on SimpleSelect

--- src/renderer/src/components/base/SimpleSelect/SimpleSelect.tsx
+++ src/renderer/src/components/base/SimpleSelect/SimpleSelect.tsx
@@ -5,16 +5,18 @@ export default function SimpleSelect({
   value = undefined,
   className = '',
   options,
+  autoFocus = false,
   onChange = () => {}
 }: {
   value: string | number | readonly string[] | undefined
   className?: string
   options: { value: string | number; label: string }[]
+  autoFocus?: boolean
   onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
 }) {
   return (
     <div className={`${className} ${styles.simpleSelect}`}>
-      <select value={value} onChange={onChange} className={styles.select}>
+      <select value={value} onChange={onChange} className={styles.select} autoFocus={autoFocus}>
         {options.map((option) => (
           <option key={option.value} value={option.value}>
             {option.label}
