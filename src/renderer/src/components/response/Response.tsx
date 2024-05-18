9056a30dcd4af5e12a605f4e7c7cde8fdefcb96d
Francesc d'Assís Requesens i Roca <francesc.requesens@gmail.com>
2024-05-18 20:22:09 +0200

    Copy response headers

--- src/renderer/src/components/response/Response.tsx
+++ src/renderer/src/components/response/Response.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useMemo, useState } from 'react'
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs'
 import ResponseBody from './ResponseBody'
 import ResponseHeaders from './ResponseHeaders'
@@ -6,6 +6,8 @@ import ResponseStatus from './ResponseStatus'
 import { RequestContext } from '../../context/RequestContext'
 import Loading from '../base/Loading/Loading'
 import styles from './Response.module.css'
+import Switch from '../base/Switch/Switch'
+import { formatSource, getLanguageName } from '../../lib/languageSupport'
 
 export default function Response({
   consoleIsHidden,
@@ -23,6 +25,12 @@ export default function Response({
   const [headers, setHeaders] = useState<KeyValue[]>([])
   const [body, setBody] = useState('')
 
+  const [showRaw, setShowRaw] = useState(true)
+  const [raw, setRaw] = useState(false)
+  const [rawValue, setRawValue] = useState('')
+  const [parsedValue, setParsedValue] = useState('')
+  const [tabIndex, setTabIndex] = useState(0)
+
   useEffect(() => {
     setFetching(context.fetching)
     setFetched(context.fetched)
@@ -31,22 +39,55 @@ export default function Response({
     setHeaders(context.response.headers)
     setBody(context.response.body)
     setSize(context.response.size)
+    setRawValue(context.response.body)
+    setParsedValue(formatSource(context.response.body))
   }, [context])
 
+  const language: string = useMemo(() => {
+    if (raw) return 'text'
+    return getLanguageName(rawValue)
+  }, [raw, rawValue])
+
+  const handleSelectTab = (index: number) => {
+    setTabIndex(index)
+    setShowRaw(index === 0)
+  }
+
+  const handleCopy = () => {
+    let valueToCopy = ''
+    if (tabIndex === 0) {
+      valueToCopy = raw ? rawValue : parsedValue
+    } else {
+      valueToCopy = headers
+        .map((header) => {
+          return `${header.name}: ${header.value}`
+        })
+        .join('\n')
+    }
+    navigator.clipboard.writeText(valueToCopy)
+  }
   return (
     <>
       {fetching && <Loading />}
       {!fetching && fetched && (
         <div className={styles.response}>
           <div className={styles.content}>
-            <Tabs className="tabs">
-              <TabList>
-                <Tab>Response</Tab>
-                <Tab>Headers</Tab>
-              </TabList>
+            <Tabs className="tabs" onSelect={handleSelectTab}>
+              <div className={styles.tabsList}>
+                <TabList>
+                  <Tab>Response</Tab>
+                  <Tab>Headers</Tab>
+                </TabList>
+                <div className={styles.responseActions}>
+                  <div className={styles.copy} onClick={handleCopy}>
+                    Copy
+                  </div>
+                  {showRaw && <Switch text="Raw" active={raw} onChange={setRaw} />}
+                </div>
+              </div>
               <div className="tab-panel-wrapper">
                 <TabPanel forceRender={true}>
-                  <ResponseBody value={body} />
+                  <ResponseBody value={body} raw={raw} language={language} />
                 </TabPanel>
                 <TabPanel async forceRender={true}>
                   <ResponseHeaders headers={headers} />
--- src/renderer/src/components/response/ResponseBody.tsx
+++ src/renderer/src/components/response/ResponseBody.tsx
@@ -1,49 +1,24 @@
-import React, { useEffect, useMemo, useState } from 'react'
-import { formatSource, getLanguageName } from '../../lib/languageSupport'
+import React from 'react'
 import Editor from '../base/Editor'
 import styles from './Response.module.css'
-import Switch from '../base/Switch/Switch'
 
-export default function ResponseBody({ value }: { value: string }) {
-  const [showRaw, setShowRaw] = useState(false)
-  const [rawValue, setRawValue] = useState('')
-  const [parsedValue, setParsedValue] = useState('')
-
-  useEffect(() => {
-    setRawValue(value)
-    setParsedValue(formatSource(value))
-  }, [value])
-
-  const language: string = useMemo(() => {
-    if (showRaw) return 'text'
-    return getLanguageName(rawValue)
-  }, [showRaw, rawValue])
-
-  const handleCopy = () => {
-    navigator.clipboard.writeText(showRaw ? rawValue : parsedValue)
-  }
-
+export default function ResponseBody({
+  value,
+  raw,
+  language
+}: {
+  value: string
+  raw: boolean
+  language: string
+}) {
   return (
-    <div className={`${rawValue.length ? styles.body : styles.bodyNoContent}`}>
-      {rawValue && (
-        <>
-          <div className={styles.bodyHeader}>
-            <div className={styles.copy} onClick={handleCopy}>
-              Copy
-            </div>
-            <Switch text="Raw" active={showRaw} onChange={setShowRaw} />
-          </div>
-
-          <div className={styles.bodyContent}>
-            <Editor
-              language={language}
-              value={showRaw ? rawValue : parsedValue}
-              wordWrap={showRaw}
-            />
-          </div>
-        </>
+    <div className={`${value.length ? styles.body : styles.bodyNoContent}`}>
+      {value && (
+        <div className={styles.bodyContent}>
+          <Editor language={language} value={value} wordWrap={raw} />
+        </div>
       )}
-      {!rawValue && <div className={styles.noContent}>No content</div>}
+      {!value && <div className={styles.noContent}>No content</div>}
     </div>
   )
 }
