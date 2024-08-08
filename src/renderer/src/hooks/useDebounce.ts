20e2386146781db85687afabbd4f78fc553b3f25
Francesc d'Assís Requesens i Roca <francesc.requesens@gmail.com>
2024-08-08 19:01:23 +0200

    refactor: Add optional delayOnFalse parameter to useDebounce hook

--- src/renderer/src/hooks/useDebounce.ts
+++ src/renderer/src/hooks/useDebounce.ts
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react'
 
-export function useDebounce(value: string | number | boolean, delay: number) {
+export function useDebounce(value: string | number | boolean, delay: number, delayOnFalse = 0) {
   const [debouncedValue, setDebouncedValue] = useState(value)
 
   useEffect(() => {
+    if (typeof value === 'boolean' && !value) {
+      const handler = setTimeout(() => setDebouncedValue(value), delayOnFalse)
+      return () => clearTimeout(handler)
+    }
     const handler = setTimeout(() => setDebouncedValue(value), delay)
     return () => clearTimeout(handler)
-  }, [value, delay])
+  }, [value, delay, delayOnFalse])
 
   return debouncedValue
 }
