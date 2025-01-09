e0784570af176235e923f4aadcc22f462e30372c
Francesc d'Assís Requesens i Roca <francesc.requesens@gmail.com>
2025-01-09 11:27:37 +0100

    feat: autosave settings

--- src/renderer/src/components/layout/sideBar/Settings/Settings.tsx
+++ src/renderer/src/components/layout/sideBar/Settings/Settings.tsx
@@ -33,9 +33,11 @@ export default function Settings() {
     setOpThemes([...opThemes])
   }, [appSettings?.settings])
 
-  const saveSettings = () => {
-    if (!settings) return
-    appSettings?.save(settings)
+  if (!settings) return null
+
+  const handleChangeSettings = (newSettings: AppSettingsType) => {
+    setSettings(newSettings)
+    appSettings?.save(newSettings)
   }
 
   const handleClearSettings = () => {
@@ -48,8 +50,6 @@ export default function Settings() {
       onCancel: () => application.hideConfirm()
     })
   }
-
-  if (!settings) return null
 
   const getThemeName = (value: string): string => {
     return value
@@ -83,7 +83,9 @@ export default function Settings() {
             <label htmlFor="theme">Theme</label>
             <SimpleSelect
               value={settings.theme}
-              onChange={(e) => setSettings({ ...settings, theme: getThemeName(e.target.value) })}
+              onChange={(e) =>
+                handleChangeSettings({ ...settings, theme: getThemeName(e.target.value) })
+              }
               options={opThemes}
               groupBy="mode"
             />
@@ -93,7 +95,7 @@ export default function Settings() {
             <SimpleSelect
               value={settings.requestView}
               onChange={(e) =>
-                setSettings({ ...settings, requestView: getRequestView(e.target.value) })
+                handleChangeSettings({ ...settings, requestView: getRequestView(e.target.value) })
               }
               options={requestViewOptions}
             />
@@ -107,7 +109,9 @@ export default function Settings() {
               min={3}
               max={100}
               placeholder="100"
-              onChange={(e) => setSettings({ ...settings, maxHistory: Number(e.target.value) })}
+              onChange={(e) =>
+                handleChangeSettings({ ...settings, maxHistory: Number(e.target.value) })
+              }
             />
           </div>
           <div className={styles.group}>
@@ -119,7 +123,9 @@ export default function Settings() {
               max={10000}
               value={settings.timeout}
               placeholder="1000"
-              onChange={(e) => setSettings({ ...settings, timeout: Number(e.target.value) })}
+              onChange={(e) =>
+                handleChangeSettings({ ...settings, timeout: Number(e.target.value) })
+              }
             />
           </div>
           <div className={styles.groupRow}>
@@ -127,7 +133,9 @@ export default function Settings() {
               id="manageCookies"
               type="checkbox"
               checked={settings.manageCookies}
-              onChange={(e) => setSettings({ ...settings, manageCookies: e.target.checked })}
+              onChange={(e) =>
+                handleChangeSettings({ ...settings, manageCookies: e.target.checked })
+              }
             />
             <label htmlFor="manageCookies">
               <span>Manage cookies</span>
@@ -138,7 +146,7 @@ export default function Settings() {
               id="toggleMenu"
               type="checkbox"
               checked={settings.menu}
-              onChange={(e) => setSettings({ ...settings, menu: e.target.checked })}
+              onChange={(e) => handleChangeSettings({ ...settings, menu: e.target.checked })}
             />
             <label htmlFor="toggleMenu">
               <span>Show menu</span>
@@ -146,7 +154,6 @@ export default function Settings() {
           </div>
 
           <div className={styles.group}>
-            <button onClick={saveSettings}>Save</button>
             <a className={`button-text ${styles.clearSettings}`} onClick={handleClearSettings}>
               Clear settings
             </a>
