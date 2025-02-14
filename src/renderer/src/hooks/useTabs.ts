af1312413efcfede712735dfca4cd9ec0208ba90
Francesc d'Assís Requesens i Roca <francesc.requesens@gmail.com>
2025-02-14 21:30:36 +0100

    fix: clear active collection element when switching to a draft tab

--- src/renderer/src/hooks/useTabs.ts
+++ src/renderer/src/hooks/useTabs.ts
@@ -155,6 +155,8 @@ export default function useTabs(
         toggleCollectionElements(collection.elements, true, path)
         collections.update(collection)
       }
+    } else {
+      setActiveRequest(null)
     }
     return newTabs
   }
